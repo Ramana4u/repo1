709bbda5d3194b856164ac3d3e6041e154729dad
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-04 15:54:26 +0530

    Update moduleEC2.tf

--- moduleEC2.tf
+++ moduleEC2.tf
@@ -2,5 +2,5 @@ provider "aws" {
   region = "us-east-2"
 }
 module "first module"{
-    source = "github.com/Ramana4u/repo1"
+    source = "git::https://github.com/Ramana4u/repo1.git?=firstbranch"
 }
