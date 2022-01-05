ecc0ad5f15b1c52bd1cdce48633a0964123a887a
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-01-05 10:21:21 +0530

    Update moduleEC2.tf

--- moduleEC2.tf
+++ moduleEC2.tf
@@ -2,5 +2,5 @@ provider "aws" {
   region = "us-east-2"
 }
 module "ec2_instance"{
-    source = "git::https://github.com/Ramana4u/test.git"
+    source = "git::https://github.com/Ramana4u/repo1.git?ref=firstbranch"
 }
