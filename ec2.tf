ed611165e1afc405f0b8ce216f09afe82385a1a6
Ramana4u <93709835+Ramana4u@users.noreply.github.com>
2022-07-07 10:28:17 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "myec2" {
-  ami           = "ami-0fb653ca2d3203ac1"
+  ami           = "ami-052efd3df9dad4825"
   instance_type = "t2.micro"
-  subnet_id   = "subnet-0ad08acb398ada09d"
-  key_name = "sshkey1"
+  subnet_id   = "subnet-01d1e39e523c42ce3"
+  key_name = "awskey1"
   user_data = "${file("userdata.sh")}"
-  security_groups = ["sg-017c097bb1674f881"]
+  security_groups = ["sg-0056d39900d57131c"]
   tags = {
     Name = "Ec2tf"
   }
